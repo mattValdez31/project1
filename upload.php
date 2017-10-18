d8cf6b0b1acdb56fc62090cd4ba89331ac1feae0
Matthew J Valdez <mjv32@afsaccess1.njit.edu>
2017-10-17 20:00:34 -0400

    added error checking

--- upload.php
+++ upload.php
@@ -5,6 +5,27 @@
 	$fileType = pathinfo($targetFile, PATHINFO_EXTENSION);
 	$ok = 1;
 
-	move_uploaded_file($FILES["uploadFile"]["tmpName"], $target_file)
+	//Check if file already exists
+	if(file_exists($targetFile))
+	{
+		echo "Sorry, file could not be uploaded because an error exists.";
+	}
+	
+	//Upload file if no error exists
+	if($ok == 0)
+	{
+		echo "Sorry, file could not be uploaded because an error exists.";
+	}
+	else
+	{
+		if (move_uploaded_file($FILES["uploadFile"]["tmpName"], $target_file))
+		{
+			echo $baseName . "has been uploaded.";
+		}
+		else
+		{
+			echo "There was an error uploading the requested file. Please try again.";
+		}
+	}
 
 ?>
