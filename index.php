49dfe06faee4363162389b60639ca0a42a0e2a18
Matthew J Valdez <mjv32@afsaccess1.njit.edu>
2017-10-20 19:12:35 -0400

    edited index.php

--- index.php
+++ index.php
@@ -98,7 +98,7 @@
 		{
 			$form = '<form action="index2.php?page=uploadform" method="post" enctype="multipart/form-data">';
 			$form .= '<input type="file" name="fileToUpload" id="fileToUpload">';
-			$form .= '<input type="submit" value="Upload Image" name="submit">';
+			$form .= '<input type="submit" value="Upload File" name="submit">';
 			$form .= '</form>';
 			$this->html .= '<h1>Upload Form</h1>';
 			$this->html .= $form;
