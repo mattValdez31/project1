18189d9385b5086ab0dbe9e0652de1963f0e420a
Matthew J Valdez <mjv32@afsaccess1.njit.edu>
2017-10-22 23:33:43 -0400

    added tableTags class

--- index.php
+++ index.php
@@ -96,7 +96,7 @@
 	{
 		public function get()
 		{
-			$form = '<form action="index2.php?page=uploadform" method="post" enctype="multipart/form-data">';
+			$form = '<form action="index.php?page=uploadForm" method="post" enctype="multipart/form-data">';
 			$form .= '<input type="file" name="fileToUpload" id="fileToUpload">';
 			$form .= '<input type="submit" value="Upload File" name="submit">';
 			$form .= '</form>';
@@ -106,46 +106,140 @@
 		
 		public function post()
 		{
-			echo 'test';
-			print_r($_FILES);
+			/*echo 'test';
+			print_r($_FILES);*/
+
+			$destDir = "uploads/";
+			$fName = $_FILES["fileToUpload"]["name"];
+			$baseName = basename($_FILES["fileToUpload"]["name"]);
+			$targetFile = $destDir . $_FILES["fileToUpload"]["name"];
+			$check = 1;
+
+			//Check if file already exists
+			if (file_exists($targetFile))
+			{
+				echo "Error: " . $fName . " already exists.";
+				$check = 0;
+			}
+
+			//upload file if it doesn't exist
+			if($check == 0)
+			{
+				echo "Sorry, file couldn't be uploaded due to an
+				error.";
+			}
+			else
+			{
+				if
+				(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile))
+				{
+					header("Location: index.php?page=htmlTable&filename=$fName");
+				}
+				else
+				{
+					echo "There was an error uploading the requested file.";
+				}
+			}
 		}
 	}
 
 	class htmlTable extends page
 	{
-		<table> //function?
-			<?php
-				$csvFile = fopen("csv sample 1.csv", "r");
-
-				$lines = fgetcsv($csvFile);
-				$len = count($lines);
-				for ($x = 0; $x < $len; $x++)
+		public function get()
+		{
+			
+			$csv = $_GET['filename'];
+			chdir('uploads');
+			$file = fopen($csv, "r");
+			htmlTags::tableFormat();
+			$row = 1;
+			while (($data=fgetcsv($file)) !== FALSE)
+			{
+				foreach($data as $value)
 				{
-					echo '<th> ' . $lines[$x] . ' </th>';
-				}
-
-				while(!feof($csvFile))
-				{
-					$lines = fgetcsv($csvFile);
-					$len = count($lines);
-
-					for ($x = 0; $x < $len; $x++)
+					if($row == 1)
 					{
-						switch($x)
-						{
-							case 0:
-								echo '<tr> <td> ' . $lines[$x] . '</td>';
-								break;
-							case ($len-1):
-								echo '<td> ' . $lines[$x] . '</td> </tr>';
-								break;
-							default:
-								echo '<td> ' . $lines[$x] . '</td>';
-						}
+						htmlTags::tableHeader($value);
+					}
+					else
+					{
+						htmlTags::tableContent($value);
 					}
 				}
-			?>
-		</table>
+				$row++;
+				htmlTags::breakTableRow();
+			}
+		fclose($file);
+		}
 	}
+			
+	class htmlTags
+	{
+		static public function headingOne($text)
+		{
+			return '<h1>' . $text . '</h1>';
+		}
+
+		static public function tableFormat()
+		{
+			echo "<table>";
+		}
+
+		static public function tableHeader($text)
+		{
+			echo '<th>' . $text . '</th>';
+		}
+
+		static public function tableContent($text)
+		{
+			echo '<td>' . $text . '</td>';
+		}
+
+		static public function breakTableRow()
+		{
+			echo '</tr>';
+		}
+	}
+			
+					
+			/*$out = '<table>';
+			$out .= '<?php';
+			//$out .= '$csvFile = fopen($_FILES["fileToUpload"]["name"], "r");';
+			$out .= '$csvFile = fopen("csv sample 1.csv", "r");';
+				$out .= '$lines = fgetcsv($csvFile);';
+				$out .= '$len = count($lines);';
+				$out .= 'for ($x = 0; $x < $len; $x++)';
+				$out .= '{';
+					//$out .= 'echo $len;';
+					//$out .= "echo '<th> ' . $lines[$x] . ' </th>';";
+					$out .= "echo '<th>  {$lines[$x]}  </th>';";
+				$out .= '}';
+
+				$out .= 'while(!feof($csvFile))';
+				$out .= '{';
+					$out .= '$lines = fgetcsv($csvFile);';
+					$out .= '$len = count($lines);';
+
+					$out .= 'for ($x = 0; $x < $len; $x++)';
+					$out .= '{';
+						$out .= 'switch($x)';
+						$out .= '{';
+							$out .= 'case 0:';
+								$out .= 'echo '<tr> <td> ' . $lines[$x] . '</td>';';
+								$out .= 'break;';
+							$out .= 'case ($len-1):';
+								$out .= 'echo '<td> ' . $lines[$x] . '</td> </tr>';';
+								$out .= 'break;';
+							$out .= 'default:';
+								$out .= 'echo '<td> ' . $lines[$x] . '</td>';';
+						$out .= '}';
+					$out .= '}';
+				$out .= '}';
+			$out .= '?>';
+			$out .= '</table>';
+
+			$this->html .= $out; 
+		}
+	}*/
 
 ?>
